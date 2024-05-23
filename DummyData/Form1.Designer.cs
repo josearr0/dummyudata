118c7c73be49ec90fa88cd3b7cef2a18f60fd17d
josearr0 <98491804+josearr0@users.noreply.github.com>
2024-05-22 18:35:29 -0700

    Add files via upload

---
+++ DummyData/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace DummyData
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.numRegistros = new System.Windows.Forms.NumericUpDown();
+            this.cbFormato = new System.Windows.Forms.ComboBox();
+            this.btnGenerar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numRegistros)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numRegistros
+            // 
+            this.numRegistros.Location = new System.Drawing.Point(12, 12);
+            this.numRegistros.Maximum = new decimal(new int[] {
+            50000,
+            0,
+            0,
+            0});
+            this.numRegistros.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numRegistros.Name = "numRegistros";
+            this.numRegistros.Size = new System.Drawing.Size(120, 20);
+            this.numRegistros.TabIndex = 0;
+            this.numRegistros.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // cbFormato
+            // 
+            this.cbFormato.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbFormato.FormattingEnabled = true;
+            this.cbFormato.Items.AddRange(new object[] {
+            "SQL",
+            "CSV",
+            "XML",
+            "JSON"});
+            this.cbFormato.Location = new System.Drawing.Point(12, 38);
+            this.cbFormato.Name = "cbFormato";
+            this.cbFormato.Size = new System.Drawing.Size(120, 21);
+            this.cbFormato.TabIndex = 1;
+            // 
+            // btnGenerar
+            // 
+            this.btnGenerar.Location = new System.Drawing.Point(12, 65);
+            this.btnGenerar.Name = "btnGenerar";
+            this.btnGenerar.Size = new System.Drawing.Size(120, 23);
+            this.btnGenerar.TabIndex = 2;
+            this.btnGenerar.Text = "Generar";
+            this.btnGenerar.UseVisualStyleBackColor = true;
+            this.btnGenerar.Click += new System.EventHandler(this.btnGenerar_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(284, 101);
+            this.Controls.Add(this.btnGenerar);
+            this.Controls.Add(this.cbFormato);
+            this.Controls.Add(this.numRegistros);
+            this.Name = "Form1";
+            this.Text = "Generador de Datos Ficticios";
+            ((System.ComponentModel.ISupportInitialize)(this.numRegistros)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        private System.Windows.Forms.NumericUpDown numRegistros;
+        private System.Windows.Forms.ComboBox cbFormato;
+        private System.Windows.Forms.Button btnGenerar;
+    }
+}
+
---
+++ DummyData/Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml;
+using Newtonsoft.Json;
+
+namespace DummyData
+{
+    public partial class Form1 : Form
+    {
+        private static readonly string[] ApellidosEspanol = { "García", "Martínez", "López", "Sánchez" };
+        private static readonly string[] ApellidosIngles = { "Smith", "Johnson", "Brown", "Taylor" };
+        private static readonly string[] ApellidosFrances = { "Martin", "Bernard", "Dubois", "Thomas" };
+        private static readonly string[] ApellidosAleman = { "Müller", "Schmidt", "Schneider", "Fischer" };
+        private static readonly string[] Nombres = { "Juan", "María", "Luis", "Ana", "Carlos", "Laura" };
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGenerar_Click(object sender, EventArgs e)
+        {
+            int cantidadRegistros = (int)numRegistros.Value;
+            string formato = cbFormato.SelectedItem.ToString();
+
+            List<Alumno> alumnos = GenerarDatosFicticios(cantidadRegistros);
+            GuardarDatos(alumnos, formato);
+        }
+
+        private List<Alumno> GenerarDatosFicticios(int cantidad)
+        {
+            Random rand = new Random();
+            List<Alumno> alumnos = new List<Alumno>();
+
+            for (int i = 0; i < cantidad; i++)
+            {
+                string apellido1 = ObtenerElementoAleatorio(rand, ApellidosEspanol, ApellidosIngles, ApellidosFrances, ApellidosAleman);
+                string apellido2 = ObtenerElementoAleatorio(rand, ApellidosEspanol, ApellidosIngles, ApellidosFrances, ApellidosAleman);
+                string nombre = Nombres[rand.Next(Nombres.Length)];
+                string correo = $"{nombre.ToLower()}.{apellido1.ToLower()}@unison.com";
+                DateTime fechaNacimiento = new DateTime(rand.Next(1990, 2010), rand.Next(1, 13), rand.Next(1, 29));
+
+                alumnos.Add(new Alumno
+                {
+                    Matricula = i + 1,
+                    Apellido1 = apellido1,
+                    Apellido2 = apellido2,
+                    Nombres = nombre,
+                    Correo = correo,
+                    FechaNacimiento = fechaNacimiento
+                });
+            }
+
+            return alumnos;
+        }
+
+        private string ObtenerElementoAleatorio(Random rand, params string[][] arreglos)
+        {
+            string[] arreglo = arreglos[rand.Next(arreglos.Length)];
+            return arreglo[rand.Next(arreglo.Length)];
+        }
+
+        private void GuardarDatos(List<Alumno> alumnos, string formato)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = $"{formato.ToUpper()} files (*.{formato.ToLower()})|*.{formato.ToLower()}";
+            saveFileDialog.Title = "Guardar Datos Generados";
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = saveFileDialog.FileName;
+
+                switch (formato.ToLower())
+                {
+                    case "sql":
+                        File.WriteAllText(filePath, GenerarSQL(alumnos));
+                        break;
+                    case "csv":
+                        File.WriteAllText(filePath, GenerarCSV(alumnos));
+                        break;
+                    case "xml":
+                        GenerarXML(alumnos, filePath);
+                        break;
+                    case "json":
+                        File.WriteAllText(filePath, GenerarJSON(alumnos));
+                        break;
+                }
+
+                MessageBox.Show("Archivo guardado exitosamente.", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private string GenerarSQL(List<Alumno> alumnos)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("INSERT INTO Alumnos (Matricula, Apellido1, Apellido2, Nombres, Correo, FechaNacimiento) VALUES");
+            for (int i = 0; i < alumnos.Count; i++)
+            {
+                Alumno alumno = alumnos[i];
+                sb.AppendFormat("({0}, '{1}', '{2}', '{3}', '{4}', '{5:yyyy-MM-dd}')",
+                    alumno.Matricula, alumno.Apellido1, alumno.Apellido2, alumno.Nombres, alumno.Correo, alumno.FechaNacimiento);
+                if (i < alumnos.Count - 1)
+                {
+                    sb.AppendLine(",");
+                }
+                else
+                {
+                    sb.AppendLine(";");
+                }
+            }
+            return sb.ToString();
+        }
+
+        private string GenerarCSV(List<Alumno> alumnos)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("Matricula,Apellido1,Apellido2,Nombres,Correo,FechaNacimiento");
+            foreach (var alumno in alumnos)
+            {
+                sb.AppendLine($"{alumno.Matricula},{alumno.Apellido1},{alumno.Apellido2},{alumno.Nombres},{alumno.Correo},{alumno.FechaNacimiento:yyyy-MM-dd}");
+            }
+            return sb.ToString();
+        }
+
+        private void GenerarXML(List<Alumno> alumnos, string filePath)
+        {
+            XmlWriterSettings settings = new XmlWriterSettings();
+            settings.Indent = true;
+
+            using (XmlWriter writer = XmlWriter.Create(filePath, settings))
+            {
+                writer.WriteStartDocument();
+                writer.WriteStartElement("Alumnos");
+
+                foreach (var alumno in alumnos)
+                {
+                    writer.WriteStartElement("Alumno");
+
+                    writer.WriteElementString("Matricula", alumno.Matricula.ToString());
+                    writer.WriteElementString("Apellido1", alumno.Apellido1);
+                    writer.WriteElementString("Apellido2", alumno.Apellido2);
+                    writer.WriteElementString("Nombres", alumno.Nombres);
+                    writer.WriteElementString("Correo", alumno.Correo);
+                    writer.WriteElementString("FechaNacimiento", alumno.FechaNacimiento.ToString("yyyy-MM-dd"));
+
+                    writer.WriteEndElement();
+                }
+
+                writer.WriteEndElement();
+                writer.WriteEndDocument();
+            }
+        }
+
+        private string GenerarJSON(List<Alumno> alumnos)
+        {
+            return Newtonsoft.Json.JsonConvert.SerializeObject(alumnos, Newtonsoft.Json.Formatting.Indented);
+        }
+
+    }
+
+    public class Alumno
+    {
+        public int Matricula { get; set; }
+        public string Apellido1 { get; set; }
+        public string Apellido2 { get; set; }
+        public string Nombres { get; set; }
+        public string Correo { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+    }
+}
